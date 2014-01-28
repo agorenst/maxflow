b8625274339a860215b0a32e3e908830fc30205f
agorenst <agorenst@cs.wisc.edu>
2014-01-28 11:11:39 -0600

    Cleaned up and commented, tested on a handful of big inputs now

--- maxflow.py
+++ maxflow.py
@@ -1,7 +1,31 @@
 import sys
 
+# Currently, we read in graphs in the DIMACS format 
+# (eg http://lpsolve.sourceforge.net/5.5/DIMACS_maxf.htm)
+#
+# Our graph model is a map from edges (tuples) to weights (doubles)
+# This is a fairly inefficient model, but it is quick and easy and works
+#
+# The algorithm is the K&T scaling algorithm
+# Concerns: correctness (not heavily tested), efficiency (in graph representation)
+# and numerical correctness (what if values are crazy floats and so forth)
+#
+# Nice simple input: http://ttic.uchicago.edu/~dbatra/research/mfcomp/
+
 def flip(edge):
     return (edge[1],edge[0])
+
+# we want the highest power of two that's upper-bounded
+# by an outgoing edge from s. (we could also bound by
+# every edge in G, but this is a cheap-and-easy improvement)
+def compute_delta(G, s):
+    max_weight = 0
+    for e in outgoing_edges(G, 0, s):
+        max_weight = max(max_weight, G[e])
+    delta = 1
+    while delta*2 < max_weight:
+        delta *= 2
+    return delta
 
 # we have delta, the scaling factor,
 # so we can treat g as its delta-scaled form
@@ -27,41 +51,37 @@ def scaled_s_t_path(g, delta, s, t):
                 if n == t:
                     break
                 wq.append(n)
+
     # now we do nice processing to make prev worth our while
     path = []
     if t not in prev:
         return path
+
+    # work backwards to construct the explicit path from s to t
     n = t
     while n != s:
         path.append((prev[n],n))
         n = prev[n]
-    path
     path.reverse()
     return path
 
 # pass-by-reference
+# Short, but important! Can't get anything wrong here.
 def augment(g, p):
-    weight = max(g.values())
     # compute the bottleneck
+    # we set weight to max possible, then restrict according to p
+    weight = max(g.values())
     for e in p:
         weight = min(weight, g[e])
+
+    # this is actually applying the flow.
     for e in p:
         g[e] -= weight
         g[flip(e)] += weight
 
-
-# we want the highest power of two that's upper-bounded
-# by an outgoing edge from s. (we could also bound by
-# every edge in G, but this is a cheap-and-easy improvement)
-def compute_delta(G, s):
-    max_weight = 0
-    for e in outgoing_edges(G, 0, s):
-        max_weight = max(max_weight, G[e])
-    delta = 1
-    while delta*2 < max_weight:
-        delta *= 2
-    return delta
-
+# The main function!
+# Note that the scaling is implemented "in-line" via
+# the delta parameter in scaled_s_t path
 def max_flow(G, s, t):
     # create a reference copy of G
     Ref = { edge: G[edge] for edge in G.keys() }
@@ -69,20 +89,21 @@ def max_flow(G, s, t):
     delta = compute_delta(G, s)
 
     while delta >= 1:
-        # this is scaling
         p = scaled_s_t_path(G, delta, s, t)
         while p != []:
             augment(G, p)
             p = scaled_s_t_path(G, delta, s, t)
         delta /= 2
 
-    # this is the flow
-    return {edge : Ref[edge] - G[edge] for edge in Ref.keys() if Ref[edge] > 0}
+    # this is the flow, only include edges with flow!
+    return {edge : Ref[edge] - G[edge] for edge in Ref.keys() if Ref[edge] - G[edge] > 0}
 
-def main():
+# returns graph G, source s, sink t, having read in their
+# description from a DIMACS-formatted file.
+def dimacs_read_graph(infile):
     G = {}
     s, t = 0, 0 # source and sink, introducing their varnames
-    for line in sys.stdin:
+    for line in infile:
         words = line.split()
         if words[0] == 'c': # comment, ignore
             pass
@@ -100,7 +121,32 @@ def main():
             assert edge not in G and flip(edge) not in G
             G[edge] = float(words[3])
             G[flip(edge)] = 0
+    return G, s, t
 
-    print max_flow(G, s, t)
+# prints out the DIMACs-formatted description of our solution flow
+def dimacs_print_flow(F,s):
+    totflow = 0
+    for e in F:
+        if e[0] == s:
+            totflow += F[e]
+    print 'c solving max flow'
+    print 'c using agorenst python solver'
+    print 'c'
+    print 's', totflow
+    print 'c'
+    print 'c Source Dest Flow'
+    # just making things sorted, pretty
+    sortedF = sorted(list(F.keys()))
+    for e in sortedF:
+        print e[0], e[1], F[e]
+    print 'c'
+    print 'c End'
 
-main()
+# read in the file from stdin, print out the result to stdout
+def main():
+    G, s, t = dimacs_read_graph(sys.stdin)
+    F = max_flow(G, s, t)
+    dimacs_print_flow(F,s)
+
+if __name__=="__main__":
+    main()
